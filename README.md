66523e6c3f7ba9d12b5dbcf93e9dabb333b7b8c2
Gabriel Zamai <47357785+GSFZamai@users.noreply.github.com>
2020-05-28 06:58:03 -0300

    Adicionado funcionalidades
    Adicionado as funcionalidades abaixo:
    - Controle de Vidas
    - Eventos onclick nos elementos
    - Dinâmica para o Game Over

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# Projeto Game Mata Mosquito
-
-Desenvolvimento do projeto Game Mata Mosquito, referente ao conteúdo de JavaScript do, [Curso Desenvolvimento Web Completo 2020](https://www.udemy.com/course/web-completo).
-
-## Recursos Utilizados
-- HTML5
-- JavaScript
+# mata_mosquito
--- assets/javascript/jogo.js
+++ assets/javascript/jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vida = 1;
 
 function ajustaPalcoJogo() {
     altura = window.innerHeight;
@@ -12,6 +13,19 @@ ajustaPalcoJogo();
 
 function posicaoAleatoria() {
 
+    //remove mosquito (caso exista)
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove();
+
+        if (vida > 3) {
+            window.location.href = './fim_de_jogo.html';
+        }else {
+            document.getElementById('v' + vida).src = './assets/imagens/coracao_vazio.png';
+
+            vida++;
+        }
+    }
+
     //define posição imagem
     var posicaoX = Math.floor(Math.random() * largura) - 90;
     var posicaoY = Math.floor(Math.random() * altura) - 90;
@@ -23,10 +37,38 @@ function posicaoAleatoria() {
     //cria elemento imagem e define propriedades
     var mosquito = document.createElement('img');
     mosquito.src = './assets/imagens/mosca.png';
-    mosquito.className = 'mosquito1';
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.left = posicaoX + 'px';
     mosquito.style.position = 'relative';
+    mosquito.id = 'mosquito';
+    mosquito.onclick = function() {
+        this.remove();
+    }
 
     document.body.appendChild(mosquito);
+}
+
+function tamanhoAleatorio() {
+    var tamanho = Math.floor(Math.random() * 3);
+    
+    switch(tamanho) {
+        case 0:
+            return 'mosquito1';
+        case 1:
+            return 'mosquito2';
+        case 2:
+            return 'mosquito3';
+    }
+}
+
+function ladoAleatorio() {
+    var lado = Math.floor(Math.random() * 2);
+
+    switch(lado) {
+        case 0:
+            return 'ladoA';
+        case 1:
+            return 'ladoB';
+    }
 }
